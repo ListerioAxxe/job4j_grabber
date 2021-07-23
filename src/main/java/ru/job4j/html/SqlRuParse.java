ce868b0f69a89dc8fba22f2c1029180deec1b999
ListerioAxxe <langua095@gmail.com>
2021-07-23 18:28:12 +0300

    2.1.1 парсинг 5 страниц

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -8,13 +8,20 @@ import org.jsoup.select.Elements;
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            Element href = td.child(0);
-            Element element = td.parent().child(5);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            System.out.println(element.text());
-        }
+        for (int i = 0; i < 5; i++) {
+            Elements listPage = doc.select(".sort_options");
+            Elements row = doc.select(".postslisttopic");
+            for (Element td : row) {
+                Element href = td.child(0);
+                Element element = td.parent().child(5);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                System.out.println(element.text());
+            }
+            Elements row2 = listPage.select("a");
+            Element hrefNextPage = row2.get(i);
+            System.out.println("------------------------------------------------------");
+            doc = Jsoup.connect(hrefNextPage.attr("href")).get();
         }
     }
+}
