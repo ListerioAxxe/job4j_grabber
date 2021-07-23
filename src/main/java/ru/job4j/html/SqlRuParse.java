ea9b8a0b196f961c092856929149b1db6a2b296f
ListerioAxxe <langua095@gmail.com>
2021-07-23 14:52:59 +0300

    Парсинг html страницы

---
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -0,0 +1,20 @@
+package ru.job4j.html;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class SqlRuParse {
+    public static void main(String[] args) throws Exception {
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        Elements row = doc.select(".postslisttopic");
+        for (Element td : row) {
+            Element href = td.child(0);
+            Element element = td.parent().child(5);
+            System.out.println(href.attr("href"));
+            System.out.println(href.text());
+            System.out.println(element.text());
+        }
+        }
+    }
