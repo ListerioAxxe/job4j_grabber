3db322791d83af10fc2997bac9d31a713e88457d
ListerioAxxe <langua095@gmail.com>
2021-07-23 14:31:15 +0300

    Job с параметрами

--- src/main/java/ru/job4j/App.java
+++ src/main/java/ru/job4j/App.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        List<Integer> list = List.of(1,2,3,4,5);
+        List<Integer> list = List.of(1, 2, 3, 4, 5);
         System.out.println(list.subList(0, 2));
     }
 }
