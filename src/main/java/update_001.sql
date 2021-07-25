7b970f4ab7176ecb9987c8e688a8a9b5334c074c
ListerioAxxe <langua095@gmail.com>
2021-07-25 17:12:47 +0300

    Запрос SQL

---
+++ src/main/java/update_001.sql
@@ -0,0 +1,17 @@
+CREATE TABLE company
+(
+    id integer NOT NULL,
+    name character varying,
+    CONSTRAINT company_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE person
+(
+    id integer NOT NULL,
+    name character varying,
+    company_id integer references company(id),
+    CONSTRAINT person_pkey PRIMARY KEY (id)
+);
+
+select p.name, c.name from person p join company c on p.company_id = c.id where c.id != 5;
+select select c.name, count(p.name) from company c join person p on p.company_id=c.id group by c.name order by 2 desc limit 1;
