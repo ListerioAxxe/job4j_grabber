ece43040bc21334431ba899e3e9621ef4c4e5e06
ListerioAxxe <langua095@gmail.com>
2021-07-25 14:09:35 +0300

    corr Post

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -68,13 +68,13 @@ public class Post {
         }
         Post post = (Post) o;
         return id == post.id && Objects.equals(title, post.title) && Objects
-                .equals(link, post.link) && Objects.equals(description, post.description) && Objects
+                .equals(link, post.link) && Objects
                 .equals(created, post.created);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, title, link, description, created);
+        return Objects.hash(id, title, link, created);
     }
 
     @Override
