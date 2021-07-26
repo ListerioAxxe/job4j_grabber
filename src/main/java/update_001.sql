94a001c021fec0b133262ea896b1563f4f03a6a2
ListerioAxxe <langua095@gmail.com>
2021-07-26 15:02:18 +0300

    Запрос SQL

--- src/main/java/update_001.sql
+++ src/main/java/update_001.sql
@@ -13,5 +13,16 @@ CREATE TABLE person
     CONSTRAINT person_pkey PRIMARY KEY (id)                                                                                              
 );                                                                                                                                       
                                                                                                                                          
-select p.name, c.name from person p join company c on p.company_id = c.id where c.id != 5;                                               
-select select c.name, count(p.name) from company c join person p on p.company_id=c.id group by c.name order by count(p.id) desc limit 1; 
+select p.name, c.name
+from person p
+join company c
+    on p.company_id = c.id
+where c.id != 5;
+
+select c.name, count(p.name)
+from company c
+join person p
+    on p.company_id=c.id
+group by c.name
+order by count(p.id)
+desc limit 1;
